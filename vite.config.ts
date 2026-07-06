518bfe041b270ddb8ff25e295cec3fec93a30cb4
sahapunkelna-boop <sahapun.kelna@gmail.com>
2026-07-06 16:27:46 +0700

    build: update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: "./",
+    base: "/Income-and-expense/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
